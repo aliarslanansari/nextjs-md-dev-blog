b1f0cadd5382f0bb5c39083108be9621fd902aeb
Ali Arsalan <aliarslan1620@gmail.com>
2022-01-14 22:56:19 +0530

    added prettier config, lint-staged hook, eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,7 +29,6 @@ module.exports = {
     "no-console": ["error", { allow: ["error"] }],
     semi: ["error", "always"],
     "@next/next/no-img-element": "off",
-    "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error"
   },
   globals: {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
-import "../styles/globals.css";
-
-import type { AppProps } from "next/app";
-
 import Header from "../app/components/Header";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
